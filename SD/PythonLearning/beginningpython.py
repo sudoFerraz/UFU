1d1c39ecc90bd948438f22d5f6f3b5305a31e675
Follow the white rabbit <rh1n0@localhost.localdomain>
2016-09-02 21:10:48 -0400

    Starting UFU repository

---
+++ SD/PythonLearning/beginningpython.py
@@ -0,0 +1,79 @@
+
+"""
+
+  ______                                   __
+ /      \                                 /  |
+/$$$$$$  |  ______   __    __   ______   _$$ |_     ______
+$$ |  $$/  /      \ /  |  /  | /      \ / $$   |   /      \
+$$ |      /$$$$$$  |$$ |  $$ |/$$$$$$  |$$$$$$/   /$$$$$$  |
+$$ |   __ $$ |  $$/ $$ |  $$ |$$ |  $$ |  $$ | __ $$ |  $$ |
+$$ \__/  |$$ |      $$ \__$$ |$$ |__$$ |  $$ |/  |$$ \__$$ |
+$$    $$/ $$ |      $$    $$ |$$    $$/   $$  $$/ $$    $$/
+ $$$$$$/  $$/        $$$$$$$ |$$$$$$$/     $$$$/   $$$$$$/
+                    /  \__$$ |$$ |
+                    $$    $$/ $$ |
+                     $$$$$$/  $$/
+"""
+
+import sys
+import socket
+import os
+
+Exchanges = ('BTC-e',)
+URLs = ('https://btc-e.com/api/3/ticker/btc_usd',)
+port = 80
+user = sys.argv[1]
+pwd = sys.argv[2]
+userlist = ('ferraz', 'root', 'dpr',)
+pwdlist = ('starwars', 'toor', 'rogers')
+
+
+
+def Hello():
+    print "[+]Bem vindo ao cryptowatcher do %s\n" % user
+    print "Temos varias opcoes pre-built e customizaveis\n"
+
+
+def Conexao():
+    opt = raw_input('[+]Digite o nro da exchange para se conectar, ou o nome para criar uma nova Conexao: ')
+    if opt == '0':
+        print "\nVoce escolheu a opcao %s" % Exchanges
+
+def Verifica_senha():
+    pwd2 = raw_input('Digite sua senha novamente: ')
+    while pwd2 != pwd:
+        pwd2 = raw_input('Senha errada, tente novamente: ')
+
+def Menu():
+    largura = input('Entre com o tamanho do menu da sua tela: ')
+    opt_largura = 10
+    item_largura = largura - opt_largura
+
+    formato_cabecalho = '%-*s%*s'
+    formata = '%-*s%*.2f'
+
+    print '-' * largura
+
+    print formata % (item_largura, 'BTC-e', opt_largura, 1)
+    print formata % (item_largura, 'Foxbit', opt_largura, 2)
+    print formata % (item_largura, 'Mtgox', opt_largura, 3)
+
+    print '=' * largura
+
+
+
+def Logado():
+    Hello()
+    print Exchanges[0] + ' : ' + URLs[0] + '\n'
+
+def Main():
+    print "\n[+]Verificando seu usuario e senha\n"
+    if user in userlist:
+        Logado()
+        Verifica_senha()
+        Menu()
+
+    else:
+        print "Seu usuario nao consta no banco de dados, tente denovo"
+
+Main()
---
+++ SD/PythonLearning/testesocket.py
@@ -0,0 +1,44 @@
+  #####
+ #     #  ####  #####  #    # ###### #####
+ #       #    # #    # #    # #      #    #
+ #  #### #    # #    # ###### #####  #    #
+ #     # #    # #####  #    # #      #####
+ #     # #    # #      #    # #      #   #
+  #####   ####  #      #    # ###### #    #
+
+
+#!/usr/bin/env python
+#Cliente simples para testes com socket com protocolo gopher
+# - testesocket.py
+
+import sys
+import socket
+
+port = 70   #Gopher utiliza a porta 70
+host = sys.argv[1]  #Chamada da funcao parametro 1
+filename = sys.argv[2]  #Metodo vem do objeto sys
+
+#Declara uma variavel S e atribui a ela um objeto socket
+#No objeto socket, o metodo .socket passa a familia de end
+#e o tipo de conexao
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(2)
+
+#Para conectar passar o objeto com o metodo .connect(("host",porta))
+try:
+    s.connect((host, port))
+except socket.timeout, e:
+    print "Error connecting to server: %s" %e
+    sys.exit(1)
+except socket.gaierror, e:
+    print "Error connecting to server: %s" %e
+    sys.exit(1)
+
+#Manda um pacote de dados nas conexoes do socket
+s.sendall(filename + "\r\n")
+
+while 1:
+    buf = s.recv(2048)
+    if not len(buf):
+        break
+    sys.stdout.write(buf)
